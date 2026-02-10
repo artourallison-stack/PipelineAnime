58ee2756d78b6e2131cfd9fb1e193b607b62ee4a
artourallison-stack <artourallison@gmail.com>
2026-02-10 16:27:15 +0400

    profiles.py

---
+++ pipeline/profiles.py
@@ -0,0 +1,11 @@
+PROFILES = {
+    "exploration": {
+        "max_active_agents": 15
+    },
+    "production": {
+        "max_active_agents": 10
+    },
+    "platform_safe": {
+        "max_active_agents": 8
+    }
+}
